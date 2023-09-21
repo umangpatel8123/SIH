04691a6876edfce4139a9fac923f862d1fa2491b
umangpatel8123 <umangpatel81203@gmail.com>
2023-09-21 17:53:28 +0530

    socket

--- app/index.js
+++ app/index.js
@@ -1,102 +1,137 @@
-import { useState, useEffect } from "react";
-import { View, ScrollView, Text, SafeAreaView, Button } from "react-native";
-import { Stack, useRouter } from "expo-router";
-import * as Location from "expo-location"; // Import Location from Expo
-
-import { COLORS, icons, images, SIZES } from "../constants";
+import React, { useState, useEffect, useRef } from "react";
 import {
-  Nearbyjobs,
-  Popularjobs,
-  ScreenHeaderBtn,
-  Welcome,
-} from "../components";
+  View,
+  ScrollView,
+  Text, // Import the Text component
+  SafeAreaView,
+  Button,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import socketIOClient from "socket.io-client";
+import * as Location from "expo-location";
 
 const Home = () => {
-  const router = useRouter();
-
   const [locationData, setLocationData] = useState(null);
+  const [joinCode, setJoinCode] = useState(""); // State for input field
 
-  // Function to handle location button press
-  const handleLocationButtonPress = async () => {
+  // Define socketRef at the top of the component
+  const socketRef = useRef(null);
+
+  const handleJoinButtonPress = () => {
+    // Handle the "Join" button press here
+    console.log("Join button pressed with code:", joinCode);
+
+    // Initialize the socket connection
+    socketRef.current = socketIOClient("http://localhost:4000"); // Replace with your server URL
+
+    // Emit the "joinRoom" event with the joinCode
+    socketRef.current.emit("joinRoom", joinCode);
+
+    // Listen for location updates
+    socketRef.current.on("locationUpdate", (data) => {
+      // Update the location data in the state
+      setLocationData(JSON.parse(data)); // Parse JSON data
+    });
+
+    // Send location data at regular intervals
+    setInterval(sendLocationData, 1000); // Send data every 10 seconds (adjust as needed)
+  };
+
+  const sendLocationData = async () => {
     try {
-      // Request location permissions.
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         console.log("Permission to access location was denied");
         return;
       }
 
-      // Fetch the user's current location.
-      const location = await Location.getCurrentPositionAsync({});
+      const location = await Location.getCurrentPositionAsync();
       console.log("User's location:", location);
 
-      // Send location data to your backend
       const { latitude, longitude } = location.coords;
-      //   await sendLocationToBackend(latitude, longitude);
 
-      //   console.log("Location data sent to backend successfully");
+      // Create a JSON object with latitude and longitude
+      const locationData = {
+        latitude: latitude,
+        longitude: longitude,
+      };
+
+      // Send location data as JSON to the server using the socketRef
+      socketRef.current.emit(
+        "locationUpdate",
+        joinCode,
+        JSON.stringify(locationData)
+      );
     } catch (error) {
       console.error("Error fetching or sending location:", error);
     }
   };
 
-  // Function to send location data to the backend
-  const sendLocationToBackend = async (latitude, longitude) => {
-    // Implement the logic to send location data to your backend here
-    // You can use fetch or any HTTP library of your choice
-
-    try {
-      await fetch("http://your-backend-api-url/update-location", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ latitude, longitude }),
-      });
-    } catch (error) {
-      console.error("Error sending location to backend:", error);
-    }
-  };
-
-  // Use useEffect to periodically send location updates
   useEffect(() => {
-    // const intervalId = setInterval(() => {
-    //   handleLocationButtonPress();
-    // }, 1000); // Send location every second
-
-    // Clear the interval when the component unmounts
+    // Clean up the socket connection when the component unmounts
     return () => {
-      clearInterval(intervalId);
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+      }
     };
   }, []);
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: COLORS.lightWhite },
-          headerShadowVisible: false,
-          headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-          ),
-          headerRight: () => (
-            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
-          ),
-          headerTitle: "",
-        }}
-      />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={{ flex: 1, padding: SIZES.medium }}>
-          {/* <Welcome />
-          <Popularjobs />
-          <Nearbyjobs /> */}
-
-          {/* Location Button */}
-          <Button title="Get My Location" onPress={handleLocationButtonPress} />
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.buttonContainer}>
+          <Button title="Get My Location" onPress={sendLocationData} />
         </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Join Code"
+            value={joinCode}
+            onChangeText={setJoinCode}
+          />
+          <Button title="Join" onPress={handleJoinButtonPress} />
+        </View>
+        {/* Wrap the locationData in a Text component */}
+        <Text style={styles.locationText}>
+          Location: {JSON.stringify(locationData)}
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  inputContainer: {
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  locationText: {
+    textAlign: "center",
+    marginTop: 20,
+    fontSize: 16,
+  },
+});
 
 export default Home;
